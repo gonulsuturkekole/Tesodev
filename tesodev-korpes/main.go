9ac17664a21a54001132a4d93ea71145e2fc4d53
bilgekarayer <bilgekarayer@hotmail.com>
2024-07-15 11:36:38 +0300

    main updated

--- tesodev-korpes/main.go
+++ tesodev-korpes/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	_ "github.com/labstack/echo/v4"
+	"os"
 	"tesodev-korpes/CustomerService/cmd"
 	"tesodev-korpes/pkg"
 	"tesodev-korpes/shared/config"
@@ -17,18 +19,38 @@ func main() {
 	}
 
 	e := echo.New()
+	if len(os.Args) < 2 {
+		panic("Please provide a service to start: customer, order, or both")
+	}
+	input := os.Args[1] // This argument specifies which service to start. This setup allows
+	// the program to determine its behavior based on user input provided at runtime.
 
-	cmd.BootCustomerService(client, e)
+	switch input {
+	case "customer":
+		cmd.BootCustomerService(client, e)
+	case "order":
+		BootOrderService(client, e)
+	case "both":
+		go cmd.BootCustomerService(client, e) //  allowing both cmd.BootCustomerService(client, e)
+		// and BootOrderService(client, e) functions to run simultaneously in the 'both' case
+		go BootOrderService(client, e)
+	default:
+		panic("Invalid input. Use 'customer', 'order', or 'both'.")
+	}
 
-	//challenge : after you create a func boot order service, manage somehow to run specific project
-	//description : when you give an input here it should look that input and boot THAT specific project
-	//if the input says "both" it should
-
-	//PS : do not forget to create and call a different column for order service and do not forget to boot order service
-	//from another port different than customer service
-
-	//orderCol, err := pkg.GetMongoCollection(client, "tesodev", "order")
-	//if err != nil {
-	//	panic(err)
-
+	// Keep the main function alive
+	select {}
 }
+
+//challenge : after you create a func boot order service, manage somehow to run specific project //switch
+// cases are used above.
+
+//description : when you give an input here it should look that input and boot THAT specific project
+//if the input says "both" it should.
+
+//PS : do not forget to create and call a different column for order service and do not forget to boot order service
+//from another port different than customer service---> .. OrderService config.go
+
+//orderCol, err := pkg.GetMongoCollection(client, "tesodev", "order")
+//if err != nil {
+//	panic(err)
