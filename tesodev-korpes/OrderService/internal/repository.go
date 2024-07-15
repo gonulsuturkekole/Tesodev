8625a6c0aac046d9a823567479e469b63d55be50
bilgekarayer <bilgekarayer@hotmail.com>
2024-07-15 14:28:30 +0300

    orderservice config added

--- tesodev-korpes/OrderService/internal/repository.go
+++ tesodev-korpes/OrderService/internal/repository.go
@@ -1,1 +1,37 @@
 package internal
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"tesodev-korpes/OrderService/internal/types"
+)
+
+type Repository struct {
+	collection *mongo.Collection
+}
+
+func NewRepository(col *mongo.Collection) *Repository {
+	return &Repository{
+		collection: col,
+	}
+}
+
+func (r *Repository) FindByID(ctx context.Context, id string) (*types.Order, error) {
+	var customer *types.Order
+	return customer, nil
+}
+
+func (r *Repository) Create(ctx context.Context, customer interface{}) error {
+	// Placeholder method
+	return nil
+}
+
+func (r *Repository) Update(ctx context.Context, id string, update interface{}) error {
+	// Placeholder method
+	return nil
+}
+
+func (r *Repository) Delete(ctx context.Context, id string) error {
+	// Placeholder method
+	return nil
+}
