14828e9507591228779a22a0348caf821b5bfab2
Melike Kutlu <kutlumelike2001@gmail.com>
2024-07-15 12:37:29 +0300

    order model/type added

--- tesodev-korpes/OrderService/internal/types/type.go
+++ tesodev-korpes/OrderService/internal/types/type.go
@@ -1,1 +1,19 @@
 package types
+
+import "time"
+
+type OrderRequestModel struct {
+	Price           float64 `bson:"price" json:"price"`
+	Stock           int     `bson:"stock" json:"stock"`
+	ShippingAddress string  `bson:"shipping_address" json:"shipping_address"`
+	PaymentMethod   string  `bson:"payment_method" json:"payment_method"`
+}
+
+type OrderResponseModel struct {
+	OrderName       string    `bson:"order_name" json:"order_name"`
+	Price           float64   `bson:"price" json:"price"`
+	ShippingAddress string    `bson:"shipping_address" json:"shipping_address"`
+	PaymentMethod   string    `bson:"payment_method" json:"payment_method"`
+	CreatedAt       time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt       time.Time `bson:"updated_at" json:"updated_at"`
+}
