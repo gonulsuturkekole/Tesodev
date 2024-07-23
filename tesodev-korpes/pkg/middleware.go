0754bf3682a600d902f1a2e8f4dd2b69e0c00566
bilgekarayer <bilgekarayer@hotmail.com>
2024-07-23 19:36:25 +0300

    main

--- tesodev-korpes/pkg/middleware.go
+++ tesodev-korpes/pkg/middleware.go
@@ -1,1 +1,23 @@
 package pkg
+
+import (
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+func CorrelationIDMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// Checking X-Correlation-Id header
+		correlationID := c.Request().Header.Get("X-Correlation-Id")
+		// if not exist , generate new UUID
+		if correlationID == "" {
+			correlationID = uuid.New().String()
+			// Add new UUID to header
+			c.Request().Header.Set("X-Correlation-Id", correlationID)
+		}
+		// Add response header too
+		c.Response().Header().Set("X-Correlation-Id", correlationID)
+		// continue
+		return next(c)
+	}
+}
